a714fc9272632f8ee8ed5ceb2559c9caaa1416f8
rociobessio <rocibessio@gmail.com>
2024-04-26 12:25:33 -0300

    Terminando vista de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,28 +3,34 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login', 
   styleUrls: ['./login.component.css'],
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-  wrapper: HTMLElement | null;
-  registerLink: HTMLElement | null;
-  loginLink: HTMLElement | null;
 
-  constructor() {
-    this.wrapper = document.querySelector('.wrapper-login');
-    this.registerLink = document.querySelector('.registrate-link');
-    this.loginLink = document.querySelector('.login-link');
+  active: boolean = false;
 
-    // Asignación del evento onclick dentro del constructor
-    if (this.registerLink) {
-      this.registerLink.onclick = () => {
-        if (this.wrapper) {
-          this.wrapper.classList.add('active');
-        }
-      };
+  /**
+   * Con esto al presionar sobre el signup/login
+   * cambiara de visualizacion para el usuario.
+   * Utilizo preventDefault para que no recargue la pagina
+   * @param event un evento
+   */
+  toggleActive(event?: Event) {
+    console.log('mostramos el register');
+    if (event) {
+      event.preventDefault();
     }
+    this.active = !this.active;
+  }
+
+  toggleDesactivate(event?: Event) {
+    console.log('ocultamos el register, mostramos login');
+    if (event) {
+      event.preventDefault();
+    }
+    this.active = !this.active;
   }
 
   ngOnInit(): void {
