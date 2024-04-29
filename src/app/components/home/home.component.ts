caa9627f41461d6830078c22b39b505b02d8536a
rociobessio <rocibessio@gmail.com>
2024-04-28 21:29:51 -0300

    Mostrando divs de home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent{
+  usuario: string = '';
+  fIngreso: string = '';
+  mostrarWelcome = true; // Variable para controlar la visibilidad de welcome/info-usuario
 
+  constructor() {}
+
+  toggleWelcomeAndUserInfo() {
+    this.mostrarWelcome = !this.mostrarWelcome;
+  }
 }
